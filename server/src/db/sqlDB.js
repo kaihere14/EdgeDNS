b889ac86cef10406785a61517c0cbb0afe2c2b53
kaihere14 <yashpalsharma9950@gmail.com>
2026-01-06 19:42:30 +0530

    fix: correct import statement for PrismaClient in sqlDB.js

--- server/src/db/sqlDB.js
+++ server/src/db/sqlDB.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.DATABASE_URL}`;
 
-
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 const connectDB = async () => {
   try {
